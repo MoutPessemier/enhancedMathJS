a861b7fc67711d1d541c0dada69a3967543a8f75
Jef Malfliet <36473447+Jef-Malfliet@users.noreply.github.com>
2020-11-18 16:15:17 +0100

    redundant else
    Like in the other files, the else is not needed.

--- functions/fibonacci.js
+++ functions/fibonacci.js
@@ -1,6 +1,6 @@
 const fibonacci = (upper) => {
   if (upper < 2) return 1;
-  else return fibonacci(upper - 1) + fibonacci(upper - 2);
+  return fibonacci(upper - 1) + fibonacci(upper - 2);
 };
 
 module.exports = fibonacci;
