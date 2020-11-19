9e867196367b81b2028645eee260ab6599f2e9af
Mout Pessemier <moutpessemier@hotmail.com>
2020-11-19 12:45:14 +0100

    added basic docs

--- functions/Matrix/transpose.js
+++ functions/Matrix/transpose.js
@@ -1,3 +1,8 @@
+/**
+ * Returns the transpose of a matrix
+ * @param {[[]]} matrix A 2d array
+ * @returns {[[]]} The transpose of the matrix
+ */
 const transpose = (matrix) => {
   return matrix[0].map((col, c) => matrix.map((row, r) => matrix[r][c]));
 };
--- functions/Probability/combination.js
+++ functions/Probability/combination.js
@@ -1,6 +1,12 @@
 const factorial = require("../factorial");
 
 // TODO: above a certain threshold, it's not accurate anymore --> find a way to make it accurate
+/**
+ * Calculates the probability of a combination with a population of n and a sample size r
+ * @param {number} n The size of the population
+ * @param {number} r The sample size
+ * @returns {number} The amount of possible combinations
+ */
 const combination = (n, r) => {
   return factorial(n) / (factorial(r) * factorial(n - r));
 };
