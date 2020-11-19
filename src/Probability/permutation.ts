ab31ef7601864ba45bea2fa54e2bb8da29ce57e7
Mout Pessemier <moutpessemier@hotmail.com>
2020-11-19 20:10:19 +0100

    documentation

--- src/Probability/permutation.ts
+++ src/Probability/permutation.ts
@@ -1,5 +1,10 @@
 import factorial from '../factorial';
 
+/**
+ * Calculates the probability of a permutation with a population of n and a sample size r
+ * @param n The total population
+ * @param r The sample size
+ */
 const permutation = (n: number, r: number): number => {
   return factorial(n) / factorial(n - r);
 };
