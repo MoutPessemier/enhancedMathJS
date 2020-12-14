5ee79aa5204b6c8bbeef1339cf9f849cf080c875
RG <36441093+reeveng@users.noreply.github.com>
2020-12-14 16:35:17 +0100

    remove return undefined
    can stay but is technically unnecessary

--- src/Probability/permutation.ts
+++ src/Probability/permutation.ts
@@ -9,7 +9,6 @@ import { isNumber, isFraction } from '../Helpers/index';
 const permutation = (n: number, r: number): number | undefined => {
   if (0 <= r && r <= n) {
     if (factorial(n) && factorial(r) && factorial(n - r)) return factorial(n)! / factorial(n - r)!;
-    return undefined;
   }
   return undefined;
 };
