5be361759e114028b44e067897fb36deb1c4458c
Mout Pessemier <moutpessemier@hotmail.com>
2023-11-17 14:04:08 +0100

    chore: update eslint + conform to updates in linting

--- src/Matrix/transpose.ts
+++ src/Matrix/transpose.ts
@@ -5,7 +5,7 @@ import { fillEmptyRows, getHighestRowLength } from '../Helpers';
  * @param {Array<any[]>} matrix A 2d array
  * @returns {Array<any[]>} The transposed matrix
  */
-const transpose = <T>(matrix: T[][]): T[][] => {
+const transpose = <T>(matrix: T[][]) => {
 	const rowlength = getHighestRowLength(matrix);
 	const filledMatrix = fillEmptyRows(matrix, rowlength, null);
 	return filledMatrix[0].map((_, colIndex) => filledMatrix.map((row) => row[colIndex]));
